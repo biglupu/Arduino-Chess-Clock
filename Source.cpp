52470f60e3c5a11da037a442097837efca30c41e
biglupu <94930997+biglupu@users.noreply.github.com>
2022-11-29 18:29:08 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,7 @@
 
 
 
-//Esitellaan LSD nayttoon tarvittavat pinnit
+//Esitellaan LCD nayttoon tarvittavat pinnit
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
